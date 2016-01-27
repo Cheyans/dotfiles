38708f24038c76a7150a5a641f348c8eb7264f0d
cheyans <cheyan.set@gmail.com>
2016-01-27 16:05:11 -0500

    include dotfiles

---
+++ .oh-my-zsh/lib/alias.zsh
@@ -0,0 +1,13 @@
+#aliases for Tmux
+alias tmux='tmux -2'
+alias ta='tmux attach -t'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias td='tmux detach -s'
+alias tkill='tmux kill-session -t'
+
+# convenience aliases for editing configs
+alias ev='vim ~/.vimrc'
+alias et='vim ~/.tmux.conf'
+alias ez='vim ~/.zshrc'
+alias eb='vim ~/.bashrc'
